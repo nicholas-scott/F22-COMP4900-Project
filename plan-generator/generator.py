0a8ec4458ccaa6d008fcce36e9be22fdfa9ff4b6
Nick Scott <46829523+nicholas-scott@users.noreply.github.com>
2022-12-18 16:59:13 -0500

    updated generator

--- plan-generator/generator.py
+++ plan-generator/generator.py
@@ -3,20 +3,22 @@ import random
 import time
 
 ##TODO:  Make the parking spots be chosen based on parking capacity.
-
+ 
 ## On run this will overwrite thisplans  file.
-# generatedPlanFile = '../plans/01_plan.xml'
-# networkXMLFile = '../networks/01-double-round-network.xml'
-
+generatedPlanFile = '../plans/01_plan.xml'
+networkXMLFile = '../networks/01-double-round-network.xml'
+ 
 # generatedPlanFile = '../plans/02_plan.xml'
 # networkXMLFile = '../networks/02-1-point-5-lane-roundabout-network.xml'
 
-generatedPlanFile = '../plans/03_plan.xml'
-networkXMLFile = '../networks/03-4-way-intersection-network.xml'
+# generatedPlanFile = '../plans/03_plan.xml'
+# networkXMLFile = '../networks/03-4-way-intersection-network.xml'
 
 # generatedPlanFile = '../plans/04_plan.xml'
 # networkXMLFile = '../networks/03-4-way-intersection-network.xml'
 
+secondsIn4Hours = 14400
+driverId = 0 
 
 randomSeed = 4900
 plansGenerated = 10
@@ -41,6 +43,30 @@ class Node:
   def __init__(self, x, y):
     self.x = x
     self.y = y
+
+def generate(id):
+    home = random.randint(0, len(homeStartIds) -1)
+    work = random.choices(parkingInd, weights=parkingProb)[0]
+    currTime = time.gmtime(id)                  ## This spawns them every second. Perhaps it can be tweeked
+    timeString = f'{currTime.tm_hour:02d}:{currTime.tm_min:02d}:{currTime.tm_sec:02d}'
+    timeSpentHours= random.randint(1, 8)        ## 1 to 8 hours
+    
+    f.write(f'\t<person id="{driverId}">\n')
+    f.write(f'\t\t<plan selected= "yes">\n')
+    f.write(f'\t\t\t<activity type="h" x="{allNodes[homeStartIds[home]].x}" y="{allNodes[homeStartIds[home]].y}" end_time="{timeString}">\n')
+    f.write(f'\t\t\t</activity>\n')
+    f.write(f'\t\t\t<leg mode="car">\n')
+    f.write(f'\t\t\t</leg>\n')
+    f.write(f'\t\t\t<activity type="w" x="{allNodes[parkingIds[work]].x}" y="{allNodes[parkingIds[work]].y}" max_dur="0{timeSpentHours}:00:00">\n')
+    f.write(f'\t\t\t</activity>\n')
+    f.write(f'\t\t\t<leg mode="car">\n')
+    f.write(f'\t\t\t</leg>\n')
+    f.write(f'\t\t\t<activity type="h" x="{allNodes[homeEndIds[home]].x}" y="{allNodes[homeEndIds[home]].y}" >\n')
+    f.write(f'\t\t\t</activity>\n')
+    f.write(f'\t\t</plan>\n')
+    f.write(f'\t</person>\n') 
+
+    
 
 ## Open network file to retrieve node information
 file1 = open(networkXMLFile, 'r')
@@ -67,26 +93,22 @@ f.write('<?xml version="1.0" encoding="utf-8"?>\n<!DOCTYPE population SYSTEM "ht
 
 random.seed(randomSeed)
 
-for id in range(plansGenerated):
-    home = random.randint(0, len(homeStartIds) -1)
-    work = random.choices(parkingInd, weights=parkingProb)[0]
-    currTime = time.gmtime(id)                  ## This spawns them every second. Perhaps it can be tweeked
-    timeString = f'{currTime.tm_hour:02d}:{currTime.tm_min:02d}:{currTime.tm_sec:02d}'
-    timeSpentHours= random.randint(1, 8)        ## 1 to 8 hours
-    f.write(f'\t<person id="{id}">\n')
-    f.write(f'\t\t<plan selected= "yes">\n')
-    f.write(f'\t\t\t<activity type="h" x="{allNodes[homeStartIds[home]].x}" y="{allNodes[homeStartIds[home]].y}" end_time="{timeString}">\n')
-    f.write(f'\t\t\t</activity>\n')
-    f.write(f'\t\t\t<leg mode="car">\n')
-    f.write(f'\t\t\t</leg>\n')
-    f.write(f'\t\t\t<activity type="w" x="{allNodes[parkingIds[work]].x}" y="{allNodes[parkingIds[work]].y}" max_dur="0{timeSpentHours}:00:00">\n')
-    f.write(f'\t\t\t</activity>\n')
-    f.write(f'\t\t\t<leg mode="car">\n')
-    f.write(f'\t\t\t</leg>\n')
-    f.write(f'\t\t\t<activity type="h" x="{allNodes[homeEndIds[home]].x}" y="{allNodes[homeEndIds[home]].y}" >\n')
-    f.write(f'\t\t\t</activity>\n')
-    f.write(f'\t\t</plan>\n')
-    f.write(f'\t</person>\n')
+
+for second in range(secondsIn4Hours):
+    currTime = time.gmtime(second)
+    if(second < 3600 and second % 3 == 0):
+        generate(second)
+        driverId += 1
+    elif(second < 7200 and second >= 3600 and second % 8 == 0):
+        generate(second)
+        driverId += 1
+    elif(second < 10800 and second >= 7200 and second % 15 == 0):
+        generate(second)
+        driverId += 1
+    elif(second % 8 == 0):
+        generate(second)
+        driverId += 1
+
 
 f.write(f'</population>')
 
